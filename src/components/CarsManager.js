0150eafda2e90f08cdc1391eacb3d1b71895f151
Paulina Dziobek <dziovek@gmail.com>
2020-04-17 00:44:51 +0200

    fetch

--- src/components/CarsManager.js
+++ src/components/CarsManager.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom';
 
 export default function CarsManager() {
 
-return null
+    const API = "http://localhost:4000";
+
+    fetch(`${API}/db`)
+        .then((response) => response.json())
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
+
+
+    return null
 }
