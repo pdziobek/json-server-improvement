3de258a838862a0f329d95d2ddf1f28f7022cce6
Paulina Dziobek <dziovek@gmail.com>
2020-04-16 21:37:33 +0200

    json server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import CarsManager from "./components/CarsManager";
+import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <div> Hello World! </div>
-  );
+    return (
+        <>
+            <div> Hello World!</div>
+            <CarsManager/>
+        </>
+    );
 }
 
 export default App;
